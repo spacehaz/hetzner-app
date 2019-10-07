ce4d5351424bb6b33a5d6dbd969f6a41b8ded762
spacehaz <spacehaz@gmail.com>
2019-10-08 01:51:56 +0300

    added fix

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hey, I\'m a Node.js app!')
 })
 
+app.get('/data', (req, res) => {
+  res.json({'hello': 'world!'})
+})
+
 app.listen(3000, () => {
   console.log('Server is up on 3000')
 })
